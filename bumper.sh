fefb1150875c790eeee65c290ba881a2f39bd2b3
Otto Giron <otto.giron@xoom.com>
2016-09-17 21:30:41 -0600

    Make sed to write changes

--- bumper.sh
+++ bumper.sh
@@ -3,7 +3,7 @@ VERSION=$1
 SHA256=$2
 BINARY_URL=$3
 FILE=$4
-sed -e "s/version.*/version \"$VERSION\"/" \
+sed -i -e "s/version.*/version \"$VERSION\"/" \
     -e "s_url.*_url \"$BINARY_URL\"_" \
     -e "s/sha256.*/sha256 \"$SHA256\"/" \
-    $FILE > $FILE
+    $FILE 
